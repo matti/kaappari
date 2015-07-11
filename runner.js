fedffd6eb836eb2bfa41757ae0838309052be25b
Mika Marttila <mika@enemy.fi>
2015-07-11 15:21:47 +0300

    Fix lint errors

--- runner.js
+++ runner.js
@@ -1,20 +1,20 @@
-var page = require('webpage').create();
+var page = require("webpage").create();
 
-var system = require('system');
+var system = require("system");
 var url = system.args[1];
 var output = system.args[2];
 var viewportSizeWXH = system.args[3];
 var cliptRectSizeTLWH = system.args[4];
 
 if ( viewportSizeWXH ) {
-  viewportSizeW = parseInt(viewportSizeWXH.split("x")[0]);
-  viewportSizeH = parseInt(viewportSizeWXH.split("x")[1]);
+  var viewportSizeW = parseInt(viewportSizeWXH.split("x")[0]);
+  var viewportSizeH = parseInt(viewportSizeWXH.split("x")[1]);
 
   page.viewportSize = { width: viewportSizeW, height: viewportSizeH };
 }
 
 if ( cliptRectSizeTLWH ) {
-  cliptRectParts = cliptRectSizeTLWH.split(",");
+  var cliptRectParts = cliptRectSizeTLWH.split(",");
 
   page.clipRect = {
     left: parseInt(cliptRectParts[0]),
@@ -22,7 +22,7 @@ if ( cliptRectSizeTLWH ) {
     width: parseInt(cliptRectParts[2]),
     height: parseInt(cliptRectParts[3])
   };
-};
+}
 
 page.open(url, function () {
   page.render(output, { format: "png" });
